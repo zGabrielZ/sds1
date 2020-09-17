bb573fba246890e7e5d5c7343614621ca7fa92b7
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2020-09-17 15:02:56 -0300

    Adicionando componente header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import './App.css'
+import Header from './components/Header'
+
 
 function App() {
   return (
-    <h1>Hello semana DevSuperorp</h1>
+    <div>
+      <Header/>   
+    </div>
   );
 }
 
